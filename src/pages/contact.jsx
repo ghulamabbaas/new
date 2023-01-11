d9b9500d48ca5cb8de137a68438ca69d4ca47103
ghulamabbaas <abaxi077@gmail.com>
2023-01-11 16:43:24 +0500

    updates

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
-import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//netlify.com/pdfjs-dist@6.2.1/build/pdf.worker.min.js`;
+
 
 
 const MyApp = () => {
@@ -17,7 +16,7 @@ const MyApp = () => {
     <div className='bg-neutral-900 w-full'>
       <div className='xl:container mx-auto'>
         <div className='max-w-max mx-auto  border-2 border-[#edbd10]'>
-          <Document file="/resume.pdf" options={{ workerSrc: "/pdf.worker.js" }} onLoadSuccess={onDocumentLoadSuccess}>
+          <Document file="/resume.pdf" imageResourcesPath="/public" options={{ workerSrc: "/pdf.worker.js" }} onLoadSuccess={onDocumentLoadSuccess}>
             <Page pageNumber={pageNumber} />
           </Document>
         </div>

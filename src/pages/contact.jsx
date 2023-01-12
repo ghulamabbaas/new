8e927ea568a30fb577a9143ddeb622022d263520
ghulamabbaas <abaxi077@gmail.com>
2023-01-12 18:47:35 +0500

    modify

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack5';
-
-
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const MyApp = () => {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
+    setLoading(false);
+  }
+
+  const loadErrorHandler = () => {
+    
   }
 
 
@@ -16,9 +21,11 @@ const MyApp = () => {
     <div className='bg-neutral-900 w-full'>
       <div className='xl:container mx-auto'>
         <div className='max-w-max mx-auto  border-2 border-[#edbd10]'>
-          <Document file="/resume.pdf" imageResourcesPath="/public" options={{ workerSrc: "/pdf.worker.js" }} onLoadSuccess={onDocumentLoadSuccess}>
+          {
+          <Document onLoadError={console.error} file="/resume.pdf" imageResourcesPath="/public" options={{ workerSrc: "/pdf.worker.js" }} onLoadSuccess={onDocumentLoadSuccess}>
             <Page pageNumber={pageNumber} />
           </Document>
+          }
         </div>
       </div>
     </div>
